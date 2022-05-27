c9a21a79cbdee777432300382531ee8deb2ea58d
kirryya <kir1987@inbox.ru>
2022-05-27 21:56:29 +0300

    loginReducer

--- src/redux/reducers/loginReducer.ts
+++ src/redux/reducers/loginReducer.ts
@@ -1,3 +1,10 @@
-export const loginReducer = () => {
+import {initialTestStateType} from "./testReducer";
 
+export const loginReducer = (state: initialTestStateType, action: any) => {
+    switch (action.type) {
+        case 'SET-CHECK':
+            return {...state, isChecked: action.isChecked};
+        default:
+            return state;
+    }
 }
