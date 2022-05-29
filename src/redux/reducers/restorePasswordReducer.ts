811f3c23251fd202c38b3d7fd3964a09f735048a
mr-Freeman-S <mr.baranok@mail.ru>
2022-05-29 21:24:59 +0300

    create thunk and API

--- src/redux/reducers/restorePasswordReducer.ts
+++ src/redux/reducers/restorePasswordReducer.ts
@@ -1,18 +1,25 @@
 //  properties type for action creators
+import {Dispatch} from "redux";
+import {AppStateType} from "../store";
+import {restorePasswordAPI} from "../../api/restorePasswordAPI";
+
 const SET_EMAIL = 'restorePassword/SET-EMAIL'
 const SET_NEW_PASSWORD = 'restorePassword/SET-NEW-PASSWORD'
 const SET_STATUS = 'restorePassword/SET-STATUS'
+const SET_ERROR = 'restorePassword/SET-ERROR'
 
 // init state
 type initialStateType = {
     email: string,
     newPassword: string
-    status: 'idle' | 'loading' | 'succeeded' | 'error'
+    status: 'idle' | 'loading' | 'succeeded' | 'error',
+    error: string
 }
 const initialStateRP: initialStateType = {
     email: '',
     newPassword: '',
-    status: 'idle'
+    status: 'idle',
+    error: ''
 }
 
 // reducer
@@ -24,6 +31,8 @@ export const restorePasswordReducer = (state = initialStateRP, action: rootActio
             return {...state, newPassword: action.password}
         case SET_STATUS:
             return {...state, status: action.status}
+        case SET_ERROR:
+            return {...state, error: action.error}
         default:
             return state
     }
@@ -38,8 +47,26 @@ export const setNewPasswordRP = (password: string) => {
 export const setStatusRP = (status: initialStateType["status"]) => {
     return {type: SET_STATUS, status} as const;
 }
+export const setErrorMessageRP = (error: string) => {
+    return {type: SET_ERROR, error} as const
+}
 // types action creators
 type setEmailType = ReturnType<typeof setEmailRP>
 type setNewPasswordType = ReturnType<typeof setNewPasswordRP>
 type setStatusType = ReturnType<typeof setStatusRP>
-type rootActionTypes = setEmailType | setNewPasswordType | setStatusType
+type setErrorMessageType = ReturnType<typeof setErrorMessageRP>
+type rootActionTypes = setEmailType | setNewPasswordType | setStatusType | setErrorMessageType
+
+//thunks
+export const sendMailRestorePassword = (email: string) => (dispatch: Dispatch, getState: () => AppStateType) => {
+    dispatch(setStatusRP("loading"))
+    dispatch(setEmailRP(email))
+    restorePasswordAPI.sendRestorePasswordEmail(email)
+        .then(response => {
+            dispatch(setStatusRP("succeeded"))
+        })
+        .catch(error => {
+            dispatch(setErrorMessageRP(error.error))
+            dispatch(setStatusRP("error"))
+        })
+}
