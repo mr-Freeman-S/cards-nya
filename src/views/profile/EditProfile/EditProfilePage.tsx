6fbc3f5507066803cd2521071a5b814904fa5aaf
Tima <tima52rus@gmail.com>
2022-05-29 21:28:51 +0300

    create logig for inputs

--- src/views/profile/EditProfile/EditProfilePage.tsx
+++ src/views/profile/EditProfile/EditProfilePage.tsx
@@ -3,21 +3,52 @@ import style from './EditProfilePage.module.css'
 import profile_photo from '../../../assets/images/profile-photo.png'
 import { AiOutlineCamera } from 'react-icons/ai'
 import { useNavigate } from 'react-router-dom'
+import SuperInputText from '../../../components/SuperInputText/SuperInputText'
+import { useAppDispatch, useAppSelector } from '../../../redux/store'
+import { editProfileTC } from '../../../redux/reducers/authReducer'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 export const EditProfilePage = () => {
-	const [nickName, setNickName] = useState('Petro')
-	const [email, setEmail] = useState('example@gmail.com')
+	const { name, email, avatar } = useAppSelector(state => state.auth)
+	const [userName, setUserName] = useState(name)
+	const [userEmail, setUserEmail] = useState(email)
 	const navigate = useNavigate()
+	const dispatch = useAppDispatch()
+
+	useEffect(() => {
+		//email: 'nya-admin5252@nya.nya', password: '11qazxcvBG'
+		axios
+			.post(
+				'auth/login',
+				{
+					email: 'nya-admin5252@nya.nya',
+					password: '11qazxcvBG',
+					rememberMe: false,
+				},
+				{
+					baseURL:
+						process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+					withCredentials: true,
+				}
+			)
+			.then(res => console.log(res.data))
+	}, [])
 
 	const photoSelected = (e: ChangeEvent<HTMLInputElement>) => {
 		const file = e.target.files
 		const reader = new FileReader()
-		// @ts-ignore
-		reader.readAsDataURL(file[0])
-		reader.onload = e => {
-			console.log(e.target?.result)
+		if (file) {
+			reader.readAsDataURL(file[0])
+			reader.onload = e => {
+				console.log(e.target?.result)
+				const avatar = e.target?.result as string
+				dispatch(editProfileTC(userName, avatar))
+			}
 		}
 	}
+
+	const userNameChange = () => {}
 
 	return (
 		<div className={style.bcg}>
@@ -25,9 +56,14 @@ export const EditProfilePage = () => {
 				<div className={style.block}>
 					<h3 className={style.title}>Personal Information</h3>
 					<div className={style.photoBlock}>
-						<img src={profile_photo} alt='profilePhoto' />
+						<img src={avatar ? avatar : profile_photo} alt='profilePhoto' />
 						<div className={style.addPhoto}>
-							<input id='addPhoto' type='file' onChange={photoSelected} />
+							<input
+								id='addPhoto'
+								type='file'
+								accept='.jpg, .jpeg, .png'
+								onChange={photoSelected}
+							/>
 							<label htmlFor='addPhoto'>
 								<AiOutlineCamera size={24} />
 							</label>
@@ -35,18 +71,28 @@ export const EditProfilePage = () => {
 					</div>
 					<div className={style.inputNickname}>
 						<span>Nickname</span>
-						<input
+						{/* <input
 							type='text'
 							value={nickName}
 							onChange={e => setNickName(e.target.value)}
+						/> */}
+						<SuperInputText
+							value={userName}
+							onChangeText={setUserName}
+							className={'n'}
 						/>
 					</div>
 					<div className={style.inputEmail}>
 						<span>Email</span>
-						<input
+						{/* <input
 							type='email'
 							value={email}
 							onChange={e => setEmail(e.target.value)}
+						/> */}
+						<SuperInputText
+							value={userEmail}
+							onChangeText={setUserEmail}
+							className={'f'}
 						/>
 					</div>
 					<div className={style.btnBlock}>
@@ -56,7 +102,9 @@ export const EditProfilePage = () => {
 						>
 							Cancel
 						</button>
-						<button className={style.saveBtn}>Save</button>
+						<button className={style.saveBtn} onClick={userNameChange}>
+							Save
+						</button>
 					</div>
 				</div>
 			</div>
