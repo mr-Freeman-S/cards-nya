0ec2d75884a808bf61f9dedf7f7f70f06dec6fc6
mr-Freeman-S <mr.baranok@mail.ru>
2022-06-01 01:57:40 +0300

    add formik instead useState in RecoverPasswordPage.tsx

--- src/views/login/RecoverPasswordPages/RecoverPasswordPage.tsx
+++ src/views/login/RecoverPasswordPages/RecoverPasswordPage.tsx
@@ -1,18 +1,28 @@
-import React, {useState} from 'react';
-import SuperInputText from "../../../components/SuperInputText/SuperInputText";
+import React from 'react';
 import SuperButton from "../../../components/SuperButton/SuperButton";
 import {NavLink, useNavigate} from "react-router-dom";
+import * as Yup from "yup"
 import {PATH} from "../../../utils/routingPath";
 import {useAppDispatch, useAppSelector} from "../../../redux/store";
 import {sendMailRestorePassword, setStatusRP} from "../../../redux/reducers/restorePasswordReducer";
+import {Field, Form, Formik, FormikHelpers} from "formik";
+
+type Values = {
+    email: string
+}
 
 export const RecoverPasswordPage = () => {
-    const {status, error} = useAppSelector(state => state.restorePasswordReducer)
+    const {status,error} = useAppSelector(state => state.restorePasswordReducer)
     const dispatch = useAppDispatch()
-    const [emailText, setEmailText] = useState<string>('')
     const navigate = useNavigate()
-    const onSubmit = () => {
-        dispatch(sendMailRestorePassword(emailText))
+    const initialState: Values = {email: ''}
+    const validationSchema = Yup.object({
+        email: Yup.string().required('Required').email('Invalid email format')
+    })
+
+    const onSubmit = (values: Values, {setSubmitting}: FormikHelpers<Values>) => {
+        dispatch(sendMailRestorePassword(values.email))
+        setSubmitting(false)
     }
     if (status === "succeeded") {
         navigate(`/${PATH.CHECK_EMAIL}`)
@@ -20,14 +30,18 @@ export const RecoverPasswordPage = () => {
     }
     return (
         <div>
-            <h1>It - incubator</h1>
-            <h3>Forgot your password</h3>
-            <SuperInputText value={emailText} onChangeText={setEmailText} placeholder="Email"/>
-            <p>Enter your email address and we will send you further instruction</p>
-            <SuperButton disabled={(status === 'loading')} onClick={onSubmit}>Send Instructions</SuperButton>
-            <span>{error}</span>
-            <p>Did you remember your password?</p>
-            <NavLink to={`/${PATH.LOGIN_PAGE}`}>Try logging in</NavLink>
-        </div>
-    );
-};
+            <Formik initialValues={initialState} onSubmit={onSubmit} validationSchema={validationSchema}>
+                {({isSubmitting}) => (
+                    <Form>
+                        <h1>It - incubator</h1>
+                        <h3>Forgot your password</h3>
+                        <Field  name="email" type='email' placeholder="Email"/>
+                        <p>Enter your email address and we will send you further instruction</p>
+                        <SuperButton type="submit" disabled={(isSubmitting)}>Send Instructions</SuperButton>
+                        <span>{error}</span>
+                        <p>Did you remember your password?</p>
+                        <NavLink to={`/${PATH.LOGIN_PAGE}`}>Try logging in</NavLink>
+                    </Form>)}
+            </Formik></div>
+    )
+}
