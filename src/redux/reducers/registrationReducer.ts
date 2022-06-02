1866b766ad5e1b985d36e7c08c8c9584aa19c48a
kirryya <kir1987@inbox.ru>
2022-06-02 16:18:36 +0300

    confirm password

--- src/redux/reducers/registrationReducer.ts
+++ src/redux/reducers/registrationReducer.ts
@@ -1,5 +1,7 @@
-import {Dispatch} from "redux";
+import {AnyAction} from "redux";
 import {registrationAPI, RegistrationParamsType} from "../../api/registrationAPI";
+import {ThunkAction} from "redux-thunk";
+import {AppStateType} from "../store";
 
 const initialState = {
     isRegistered: false,
@@ -26,19 +28,21 @@ export const setErrorAC = (error: ErrorType) => ({type: 'registration/SET-ERROR'
 export const setLoadingAC = (isLoading: boolean) => ({type: 'registration/SET-LOADING', isLoading} as const)
 
 // thunks
-export const registerTC = (data: RegistrationParamsType) => (dispatch: Dispatch<ActionsType>) => {
-    dispatch(setLoadingAC(true))
-    registrationAPI.registration(data)
-        .then(() => {
-            dispatch(setIsRegisteredAC(true))
-        })
-        .catch((error) => {
-            dispatch(setErrorAC(error.response.data.error));
-        })
-        .finally(() => {
-            dispatch(setLoadingAC(false))
-        })
-}
+export const registerTC = (data: RegistrationParamsType, resetForm: () => void): ThunkAction<void, AppStateType, unknown, AnyAction> =>
+    (dispatch) => {
+        dispatch(setLoadingAC(true))
+        registrationAPI.registration(data)
+            .then(() => {
+                dispatch(setIsRegisteredAC(true))
+            })
+            .catch((error) => {
+                dispatch(setErrorAC(error.response.data.error));
+            })
+            .finally(() => {
+                dispatch(setLoadingAC(false))
+                resetForm()
+            })
+    }
 
 // types
 type ActionsType = ReturnType<typeof setIsRegisteredAC>
