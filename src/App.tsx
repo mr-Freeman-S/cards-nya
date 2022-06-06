193b0dc0b35c3d7293beda29307bfec0c9332ae9
mr-Freeman-S <mr.baranok@mail.ru>
2022-06-06 19:25:25 +0300

    add conditional rendering in app.tsx with button logout

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import { PrivateRoute } from './components/Routing/PrivateRoute'
 import SuperButton from "./components/SuperButton/SuperButton";
-import {useAppDispatch} from "./redux/store";
+import {useAppDispatch, useAppSelector} from "./redux/store";
 import {useEffect} from "react";
 import {authMe, logout} from "./redux/reducers/loginReducer";
 
 function App() {
+	const isAuth = useAppSelector(state => state.login.isLogged)
 	const dispatch = useAppDispatch()
 	const logoutButtonHandler = () => {
 		dispatch(logout())
@@ -14,11 +15,11 @@ function App() {
 	}
 	useEffect(()=> {
 	dispatch(authMe())
-	},[])
+	},[dispatch])
 	return (
 		<div>
 			<Navbar />
-			<SuperButton onClick={logoutButtonHandler}>Logout</SuperButton>
+			{isAuth && <SuperButton onClick={logoutButtonHandler}>Logout</SuperButton>}
 			<PrivateRoute />
 		</div>
 	)
