9625083beecc62ef6684294d1d0b1e230838b989
Tima <tima52rus@gmail.com>
2022-06-08 21:34:03 +0300

    create types file

---
+++ src/utils/types.ts
@@ -0,0 +1,52 @@
+import {CardsType} from "../api/cardsAPI";
+
+export type CardPacksType = {
+    _id: string
+    user_id: string
+    user_name: string
+    private: boolean
+    name: string
+    path: string
+    grade: number
+    shots: number
+    cardsCount: number
+    type: string
+    rating: number
+    created: string
+    updated: string
+    more_id: string
+    __v: number
+}
+
+export type ResponsePacksType = {
+    cardPacks: CardPacksType[]
+    page: number
+    pageCount: number
+    cardPacksTotalCount: number
+    minCardsCount: number
+    maxCardsCount: number
+    token: string
+    tokenDeathTime: number
+}
+
+export type CardType = {
+    answer: string
+    question: string
+    cardsPack_id: string
+    grade: number
+    shots: number
+    user_id: string
+    created: string
+    updated: string
+    _id: string
+}
+
+export type ResponseCardsType = {
+    cardPacks: CardsType[]
+    cardsTotalCount: number
+    maxGrade: number
+    minGrade: number
+    page: number
+    pageCount: number
+    packUserId: string
+}
