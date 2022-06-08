fe4a1077ed07ad3c0a54b8a178370a7bd90a20f9
Tima <tima52rus@gmail.com>
2022-06-08 21:33:33 +0300

    upd

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import {useEffect, useState} from 'react'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
@@ -6,112 +6,72 @@ import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
+import {AiOutlineArrowUp} from 'react-icons/ai'
+import {cardsAPI} from "../../api/cardsAPI";
+import style from './Table.module.css'
+import {changeSortCardsTC, fetchCardsTC} from "../../redux/reducers/cardsReducer";
+import {useAppDispatch, useAppSelector} from "../../redux/store";
+
 
 const colums = ['Name', 'Cards', 'Last Updated', 'Created by', 'Actions']
 
-const rows = [
-	{
-		cardsCount: 0,
-		created: "2021-07-07T15:11:20.385Z",
-		grade: 0,
-		more_id: "60e5c327feaa5207a029a59c",
-		name: "no Name",
-		path: "/def",
-		private: false,
-		rating: 0,
-		shots: 0,
-		type: "pack",
-		updated: "2021-07-07T15:11:20.385Z",
-		user_id: "60e5c327feaa5207a029a59c",
-		user_name: "123nbvvcxzlkjh64@2swi.com",
-		__v: 0,
-		_id: "60e5c418feaa5207a029a5b8",
-	},
-	{
-		cardsCount: 0,
-		created: "2021-07-07T15:11:17.927Z",
-		grade: 0,
-		more_id: "60e5c327feaa5207a029a59c",
-		name: "no Name",
-		path: "/def",
-		private: false,
-		rating: 0,
-		shots: 0,
-		type: "pack",
-		updated: "2021-07-07T15:11:17.927Z",
-		user_id: "60e5c327feaa5207a029a59c",
-		user_name: "123nbvvcxzlkjh64@2swi.com",
-		__v: 0,
-		_id: "60e5c415feaa5207a029a5b7",},
-
-	{
-		cardsCount: 0,
-		created: "2021-07-07T15:11:20.385Z",
-		grade: 0,
-		more_id: "60e5c327feaa5207a029a59c",
-		name: "no Name",
-		path: "/def",
-		private: false,
-		rating: 0,
-		shots: 0,
-		type: "pack",
-		updated: "2021-07-07T15:11:20.385Z",
-		user_id: "60e5c327feaa5207a029a59c",
-		user_name: "123nbvvcxzlkjh64@2swi.com",
-		__v: 0,
-		_id: "60e5c418feaa5207a029a5b8",
-	},
-	{
-		cardsCount: 0,
-		created: "2021-07-07T15:11:20.385Z",
-		grade: 0,
-		more_id: "60e5c327feaa5207a029a59c",
-		name: "no Name",
-		path: "/def",
-		private: false,
-		rating: 0,
-		shots: 0,
-		type: "pack",
-		updated: "2021-07-07T15:11:20.385Z",
-		user_id: "60e5c327feaa5207a029a59c",
-		user_name: "123nbvvcxzlkjh64@2swi.com",
-		__v: 0,
-		_id: "60e5c418feaa5207a029a5b8",
-	}
-]
+type sortType = 'asc' | 'desc';
 
 export function Tables() {
+    // const [rows, setRows] = useState<any>([])
+    const rows = useAppSelector(state => state.cards.cardPacks)
+    const dispatch = useAppDispatch()
+    const [sortBy, setSortBy] = useState<sortType>('desc')
 
-	return (
-		<TableContainer style={{width: 850, margin: '0 auto',}} component={Paper}>
-			<Table sx={{ width: 850 }} aria-label='simple table'>
-				<TableHead>
-					<TableRow sx={{backgroundColor: '#ECECF9'}}>
-						{
-							colums && colums.map(el => {
-								return <TableCell align={"center"}>{el}</TableCell>
-							})
-						}
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{rows.map(row => (
-						<TableRow
-							key={row.name}
-							sx={{ '&:last-child td, &:last-child th': { border: '' }, '&:nth-child(even)': {backgroundColor: '#F8F7FD'} }}
-						>
-							<TableCell align={"center"} component='th' scope='row'>
-								{row.name}
-							</TableCell>
-							<TableCell align='center'>{row.cardsCount}</TableCell>
-							<TableCell align='center'>{row.updated}</TableCell>
-							<TableCell align='center'>{row.user_name}</TableCell>
-							<TableCell align='center'>{111}</TableCell>
-						</TableRow>
-					))}
-				</TableBody>
-			</Table>
-		</TableContainer>
-	)
+    useEffect(() => {
+        // cardsAPI.getCards({}).then(res => {
+        //     setRows(res.data.cardPacks)
+        //})
+        dispatch(fetchCardsTC())
+    }, []);
+
+    const changeSortHandler = () => {
+        sortBy === 'asc' ? setSortBy('desc') : setSortBy('asc')
+        dispatch(changeSortCardsTC(sortBy))
+    }
+
+
+    return (
+        <TableContainer style={{width: 850, margin: '0 auto',}} component={Paper}>
+            <Table sx={{width: 850}} aria-label='simple table'>
+                <TableHead>
+                    <TableRow sx={{backgroundColor: '#ECECF9'}}>
+                        {
+                            colums && colums.map((el, i) => {
+                                return (el === 'Last Updated' ?
+                                    <TableCell className={style.click} key={`${el}_${i}`} onClick={() => changeSortHandler()} align={"center"}>
+                                        {el}<AiOutlineArrowUp style={sortBy === 'asc' ? {transform: 'rotate(180deg)'} : {}}/>
+                                    </TableCell> : <TableCell key={`${el}_${i}`} align={"center"}>{el}</TableCell>)
+                            })
+                        }
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows && rows.map((row: any) => (
+                        <TableRow
+                            key={row.name}
+                            sx={{
+                                '&:last-child td, &:last-child th': {border: ''},
+                                '&:nth-of-type(2)': {backgroundColor: '#F8F7FD'}
+                            }}
+                        >
+                            <TableCell align={"center"} component='th' scope='row'>
+                                {row.name}
+                            </TableCell>
+                            <TableCell align='center'>{row.cardsCount}</TableCell>
+                            <TableCell align='center'>{row.updated}</TableCell>
+                            <TableCell align='center'>{row.user_name}</TableCell>
+                            <TableCell align='center'>{111}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
 }
 
