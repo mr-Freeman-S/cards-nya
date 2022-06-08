afb273f5cd6d84ad03221a7cda7ed2cccb9509d8
Vitalex951 <vitalex951@gmail.com>
2022-06-08 18:32:26 +0300

    add Preloader

---
+++ src/components/Preloader/Preloader.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './Preloader.module.css'
+
+
+export const Preloader = () => {
+    return (
+        <div className={style.loading}>
+            <div className={style.dot}></div>
+            <div className={style.dot}></div>
+            <div className={style.dot}></div>
+            <div className={style.dot}></div>
+            <div className={style.dot}></div>
+        </div>
+    );
+};
