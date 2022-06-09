d9f5f36acb65d503d55a77cb3028d535443a4223
Tima <tima52rus@gmail.com>
2022-06-09 23:33:48 +0300

    added Table component

--- src/views/packList/PackListContainer.tsx
+++ src/views/packList/PackListContainer.tsx
@@ -2,11 +2,11 @@ import React, {useEffect} from 'react';
 import {useAppDispatch, useAppSelector} from "../../redux/store";
 import {getCardPackTC} from "../../redux/reducers/packsCardReducer";
 import {Preloader} from "../../components/Preloader/Preloader";
-import {PacksList} from "./PacksList";
-import {Navigate} from "react-router-dom";
+import {Navigate, useNavigate} from "react-router-dom";
 import {PATH} from "../../utils/routingPath";
 import {PaginationCards} from "./pagination/Pagination";
 import {ButtonsShowCards} from "./buttonsShowCards/ButtonsShowCards";
+import {TablePacks} from "../../components/TablePacks/TablePacks";
 
 export const PackListContainer = () => {
     const packs = useAppSelector(state => state.packsCard.cardPacks)
@@ -14,6 +14,7 @@ export const PackListContainer = () => {
     const pageCount = useAppSelector(state => state.packsCard.pageCount)
     const packsStatus = useAppSelector(state => state.packsCard.packsStatus)
     const user_id = useAppSelector(state => state.packsCard.user_id)
+    const navigate = useNavigate()
 
     const isLogged = useAppSelector(state => state.login.isLogged)
 
@@ -21,7 +22,7 @@ export const PackListContainer = () => {
 
     useEffect(() => {
         dispatch(getCardPackTC())
-    }, [page, pageCount, user_id])
+    }, [dispatch, page, pageCount, user_id])
 
     if (!isLogged) {
         return <Navigate to={PATH.LOGIN_PAGE}/>
@@ -44,10 +45,11 @@ export const PackListContainer = () => {
             }}>
                 {packsStatus === 'loading' && <Preloader/>}
 
-                {packsStatus !== 'loading' && (<PacksList packs={packs}/>)}
+                {packsStatus !== 'loading' && (<TablePacks rows={packs}/>)}
             </div>
             <ButtonsShowCards/>
             <div style={{flex: '1 1 auto'}}>
+
                 <PaginationCards/>
             </div>
         </div>
