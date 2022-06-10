f6c3e1dfedba45a674ce3fa53331e948c24fcc6d
kirryya <kir1987@inbox.ru>
2022-06-10 22:55:35 +0300

    DeBounce

--- src/components/Search/SearchPack.tsx
+++ src/components/Search/SearchPack.tsx
@@ -1,19 +1,20 @@
 import React, {ChangeEvent, useState} from "react";
 import {DebounceInput} from "react-debounce-input";
-import {useAppDispatch} from "../../redux/store";
+import {useAppDispatch, useAppSelector} from "../../redux/store";
 import {getCardPackTC, searchPackAC} from "../../redux/reducers/packsCardReducer";
 
 
 export const SearchPack = () => {
 
+    const packName = useAppSelector<string>(state => state.packsCard.packName)
     const dispatch = useAppDispatch()
 
-    const [packName, setPackName] = useState<string>("")
+    const [value, setValue] = useState<string>(packName)
 
     const searchPacksHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value)
         dispatch(searchPackAC(e.target.value))
         dispatch(getCardPackTC())
-        setPackName(packName)
     }
 
     return (
@@ -22,7 +23,7 @@ export const SearchPack = () => {
                 minLength={2}
                 debounceTimeout={2000}
                 placeholder={"Search Pack"}
-                value={packName}
+                value={value}
                 onChange={searchPacksHandler}
             />
         </div>
